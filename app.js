acf3fcb607c9badc13dc1e0e47d182e19688d5ac
jackgrossman <99386295+jackgrossman@users.noreply.github.com>
2022-06-27 14:44:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3005); 
+app.set('port', process.env.PORT || 3000); 
 app.set('views', __dirname + '/app/server/views'); 
 app.set('view engine', 'ejs'); 
 app.use(express.static(__dirname + '/app/public')); 
